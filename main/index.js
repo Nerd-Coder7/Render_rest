37d46fc0bbe27f23eb233bed37d8a91d26800e15
Nerd-Coder7 <126475820+Nerd-Coder7@users.noreply.github.com>
2023-05-04 13:36:02 +0530

    Update index.js

--- main/index.js
+++ main/index.js
@@ -6,6 +6,18 @@ app.use(express.json());
 app.get("/shivam",(req,res)=>{
         res.send("Shivam you are a good boy")
         })
+
+app.get("/ritul",(req,res)=>{
+        res.send("Ritul you are a good boy")
+        })
+
+app.get("/ankit",(req,res)=>{
+        res.send("Ankit you are a bhabhi boy")
+        })
+
+app.get("/",(req,res)=>{
+        res.send("use Get /shivam,ritul,ankit routes")
+        })
 app.listen(8080,()=>{
   console.log("Server is listening on 8080")
 })
